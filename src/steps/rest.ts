NordicSemiconductor/cloud-e2e-bdd-test-runner-js

commit e935724393bf826251fbb52f9991b0bda5df4720

fix: filter out nulls in body.items

src/steps/rest.ts
@@ -11,6 +11,16 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
     willRun: regexMatcher(rx),
     run,
   });
+  // When using a transform in a query the results can contain
+  // null values (items that didn't match the transform), which
+  // can cause random failures when testing for equality. This
+  // function removes the nulls from body.items.
+  const filterOutNulls = (body: any) => {
+    if (body.items && body.items.length > 0) {
+      body.items = body.items.filter((item: any) => item !== null);
+    }
+    return body;
+  };
   return [
     s(/^the ([^ ]+) header is "([^"]+)"$/, async ([name, value]) => {
       client.headers[name] = value;
@@ -53,7 +63,8 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
     }),
     s(/^"([^"]+)" of the response body is not empty$/, async ([exp]) => {
       const e = jsonata(exp);
-      const v = e.evaluate(client.response.body);
+      const body = filterOutNulls(client.response.body);
+      const v = e.evaluate(body);
       expect(v).to.not.be.an('undefined');
       return v;
     }),
@@ -61,7 +72,8 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
       /^"([^"]+)" of the response body should equal "([^"]+)"$/,
       async ([exp, expected]) => {
         const e = jsonata(exp);
-        const v = e.evaluate(client.response.body);
+        const body = filterOutNulls(client.response.body);
+        const v = e.evaluate(body);
         expect(v).to.equal(expected);
         return v;
       },
@@ -70,7 +82,8 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
       /^"([^"]+)" of the response body should equal ([0-9]+)$/,
       async ([exp, expected]) => {
         const e = jsonata(exp);
-        const v = e.evaluate(client.response.body);
+        const body = filterOutNulls(client.response.body);
+        const v = e.evaluate(body);
         expect(v).to.equal(+expected);
         return v;
       },
@@ -83,7 +96,8 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
         }
         const j = JSON.parse(step.interpolatedArgument);
         const e = jsonata(exp);
-        const v = e.evaluate(client.response.body);
+        const body = filterOutNulls(client.response.body);
+        const v = e.evaluate(body);
         expect(v).to.deep.equal(j);
         return v;
       },

commit 55710ca0844e4343f6e095fa020ac2a444c6e2bb

fix: revert breaking changes
This reverts the breaking changes introduced in
e935724393bf826251fbb52f9991b0bda5df4720

src/steps/rest.ts
@@ -11,21 +11,6 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
     willRun: regexMatcher(rx),
     run,
   });
-  // When using a transform in a query the results can contain
-  // null values (items that didn't match the transform), which
-  // can cause random failures when testing for equality. This
-  // function removes the nulls from body.items.
-  const filterOutNulls = (body: any) => {
-    if (body.items && body.items.length > 0) {
-      const originalItemsCount = body.items.length;
-      body.items = body.items.filter((item: any) => item !== null);
-      console.log(
-        `filterOutNulls removed ${originalItemsCount -
-          body.items.length} null items`,
-      );
-    }
-    return body;
-  };
   return [
     s(/^the ([^ ]+) header is "([^"]+)"$/, async ([name, value]) => {
       client.headers[name] = value;
@@ -68,9 +53,7 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
     }),
     s(/^"([^"]+)" of the response body is not empty$/, async ([exp]) => {
       const e = jsonata(exp);
-      const body = filterOutNulls(client.response.body);
-      console.log(body);
-      const v = e.evaluate(body);
+      const v = e.evaluate(client.response.body);
       expect(v).to.not.be.an('undefined');
       return v;
     }),
@@ -78,9 +61,7 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
       /^"([^"]+)" of the response body should equal "([^"]+)"$/,
       async ([exp, expected]) => {
         const e = jsonata(exp);
-        const body = filterOutNulls(client.response.body);
-        console.log(body);
-        const v = e.evaluate(body);
+        const v = e.evaluate(client.response.body);
         expect(v).to.equal(expected);
         return v;
       },
@@ -89,9 +70,7 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
       /^"([^"]+)" of the response body should equal ([0-9]+)$/,
       async ([exp, expected]) => {
         const e = jsonata(exp);
-        const body = filterOutNulls(client.response.body);
-        console.log(body);
-        const v = e.evaluate(body);
+        const v = e.evaluate(client.response.body);
         expect(v).to.equal(+expected);
         return v;
       },
@@ -104,9 +83,7 @@ export const restStepRunners = <W extends Store>(): StepRunner<W>[] => {
         }
         const j = JSON.parse(step.interpolatedArgument);
         const e = jsonata(exp);
-        const body = filterOutNulls(client.response.body);
-        console.log(body);
-        const v = e.evaluate(body);
+        const v = e.evaluate(client.response.body);
         expect(v).to.deep.equal(j);
         return v;
       },