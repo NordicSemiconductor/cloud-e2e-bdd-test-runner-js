be691ac0f4418c0f02d6440ed8039a191a505a53
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-01-05 08:32:08 +0100

    feat: upgrade to AWS SDK v3 (#289)
    See https://github.com/bifravst/aws/issues/818
    
    BREAKING CHANGE: Requires use of AWS SDK v3

--- src/lib/webhook-receiver.ts
+++ src/lib/webhook-receiver.ts
@@ -1,4 +1,8 @@
-import { SQS } from 'aws-sdk'
+import {
+	DeleteMessageCommand,
+	ReceiveMessageCommand,
+	SQSClient,
+} from '@aws-sdk/client-sqs'
 import { FeatureRunner } from './runner'
 
 type WebhookRequest = {
@@ -11,12 +15,12 @@ type WebhookRequest = {
  */
 export class WebhookReceiver {
 	private readonly queueUrl: string
-	private readonly sqs: SQS
+	private readonly sqs: SQSClient
 	latestWebhookRequest?: WebhookRequest
 
 	constructor(queueUrl: string, region: string) {
 		this.queueUrl = queueUrl
-		this.sqs = new SQS({ region })
+		this.sqs = new SQSClient({ region })
 	}
 
 	/**
@@ -32,29 +36,34 @@ export class WebhookReceiver {
 		MessageGroupId: string,
 		runner: FeatureRunner<any>,
 	): Promise<WebhookRequest> {
-		const { Messages } = await this.sqs
-			.receiveMessage({
+		const { Messages } = await this.sqs.send(
+			new ReceiveMessageCommand({
 				QueueUrl: this.queueUrl,
 				MaxNumberOfMessages: 1,
 				MessageAttributeNames: ['All'],
 				AttributeNames: ['MessageGroupId'],
 				WaitTimeSeconds: 20,
-			})
-			.promise()
+			}),
+		)
+
 		if (Messages === undefined || !Messages.length) {
 			throw new Error('No webhook request received!')
 		}
 		const { Body, MessageAttributes, ReceiptHandle, Attributes } = Messages[0]
-		await this.sqs
-			.deleteMessage({
+		await this.sqs.send(
+			new DeleteMessageCommand({
 				QueueUrl: this.queueUrl,
 				ReceiptHandle: ReceiptHandle as string,
-			})
-			.promise()
-		const attrs = MessageAttributes as SQS.Types.MessageBodyAttributeMap
-		const {
-			MessageGroupId: RcvdMessageGroupId,
-		} = Attributes as SQS.Types.MessageSystemAttributeMap
+			}),
+		)
+
+		if (Attributes === undefined || MessageAttributes === undefined)
+			throw new Error(
+				`No attributes defined in Message "${JSON.stringify(Messages[0])}"!`,
+			)
+
+		const attrs = MessageAttributes
+		const { MessageGroupId: RcvdMessageGroupId } = Attributes
 		this.latestWebhookRequest = {
 			headers: Object.keys(attrs ?? {}).reduce(
 				(hdrs: { [key: string]: string }, key) => {
@@ -81,22 +90,23 @@ export class WebhookReceiver {
 	 * Deletes all messages in a Queue instead of using purge (which can only be used every 60 seconds)
 	 */
 	async clearQueue(): Promise<void> {
-		const { Messages } = await this.sqs
-			.receiveMessage({
+		const { Messages } = await this.sqs.send(
+			new ReceiveMessageCommand({
 				QueueUrl: this.queueUrl,
 				MaxNumberOfMessages: 10,
 				WaitTimeSeconds: 0,
-			})
-			.promise()
+			}),
+		)
+
 		if (Messages !== undefined) {
 			await Promise.all(
 				Messages.map(async ({ ReceiptHandle }) =>
-					this.sqs
-						.deleteMessage({
+					this.sqs.send(
+						new DeleteMessageCommand({
 							QueueUrl: this.queueUrl,
 							ReceiptHandle: ReceiptHandle as string,
-						})
-						.promise(),
+						}),
+					),
 				),
 			)
 			await this.clearQueue()
